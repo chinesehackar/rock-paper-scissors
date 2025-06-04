7acdb15c583c36563ffbf869bb1820fd2ffc0c62
chinesehackar <212491917+chinesehackar@users.noreply.github.com>
2025-06-04 12:58:18 +0800

    Finish playRound function
    Allows for determining the winner on a per-round basis. toLowerCase() was pushed back to the getHumanChoice function

--- script.js
+++ script.js
@@ -22,7 +22,39 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   let playerChoice = prompt("rock, paper, scissors: ")
-  return playerChoice;
+  return playerChoice.toLowerCase();
 }
 
-console.log(getHumanChoice())
+function playRound(humanChoice, computerChoice) {
+  console.log(`You chose ${humanChoice} and the computer chose ${computerChoice}`);
+  if (humanChoice === computerChoice) {
+    console.log("It's a Tie!");
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose! Paper beats Rock.");
+    computerScore++;
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win! Rock beats Scissors.");
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You win! Paper beats rock.");
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("You lose! Scissors beat paper");
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You lose! Rock beats scissors");
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You win! Scissors beat paper");
+    computerScore++;
+  } else {
+    console.log("Game invalidated");
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+
